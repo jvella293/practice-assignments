b555f988adf555a068998b2933aee3f9cc714e98
Tom van Woudenberg <t.r.vanwoudenberg@tudelft.nl>
2025-02-13 17:26:29 +0100

    Added solutions wokrshop 1

--- matrixmethod/elements.py
+++ matrixmethod/elements.py
@@ -68,14 +68,14 @@ class Element:
 
         self.L = np.sqrt((self.nodes[1].x - self.nodes[0].x)**2.0 + (self.nodes[1].z - self.nodes[0].z)**2.0)
 
-        alpha = np.arctan2 #YOUR CODE HERE
+        alpha = np.arctan2( - (self.nodes[1].z - self.nodes[0].z) , (self.nodes[1].x - self.nodes[0].x))
 
         T = np.zeros((6, 6))
 
-        T[0, 0] = T[1, 1] = T[3, 3] = T[4, 4] #YOUR CODE HERE
-        T[0, 1] = T[3, 4] #YOUR CODE HERE
-        T[1, 0] = T[4, 3] #YOUR CODE HERE
-        T[2, 2] = T[5, 5] #YOUR CODE HERE
+        T[0, 0] = T[1, 1] = T[3, 3] = T[4, 4] = np.cos(alpha)
+        T[0, 1] = T[3, 4] = -np.sin(alpha)
+        T[1, 0] = T[4, 3] = np.sin(alpha)
+        T[2, 2] = T[5, 5] = 1
 
         self.T = T
         self.Tt = np.transpose(T)
@@ -129,7 +129,19 @@ class Element:
         EI = self.EI
         L = self.L
 
-        #YOUR CODE HERE
+        # Extension contribution
+
+        k[0, 0] = k[3, 3] = EA / L
+        k[3, 0] = k[0, 3] = -EA / L
+
+        # Bending contribution
+
+        k[1, 1] = k[4, 4] = 12.0 * EI / L / L / L
+        k[1, 4] = k[4, 1] = -12.0 * EI / L / L / L
+        k[1, 2] = k[2, 1] = k[1, 5] = k[5, 1] = -6.0 * EI / L / L
+        k[2, 4] = k[4, 2] = k[4, 5] = k[5, 4] = 6.0 * EI / L / L
+        k[2, 2] = k[5, 5] = 4.0 * EI / L
+        k[2, 5] = k[5, 2] = 2.0 * EI / L
 
         return np.matmul(np.matmul(self.Tt, k), self.T)
 
